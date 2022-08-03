9a6afb89738a6dbc1228002d2d4b71890b85b4c5
Cesar-DENOUE <ces.denoue@gmail.com>
2022-08-03 18:58:32 +0200

    commit

--- index.php
+++ index.php
@@ -8,7 +8,12 @@
 	<title></title>
 	
 	<link rel="stylesheet" href="assets/styles/main.css">
-	
+	< ! couleurs : #efefef (blanc cassé)
+	#aeafb0	(texte)
+	#265be6 (bleu)
+	#668cf0 (bleu ombre)
+	#63b168 (vert)
+	#d7eedb (vert clair) >
 </head>
 <body>
 
